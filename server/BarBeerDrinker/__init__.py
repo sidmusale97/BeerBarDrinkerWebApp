04971997221900a86dcd9ceb78114b8873a5f811
sidmusale97 <ssm168@rutgers.edu>
2018-11-17 20:02:08 -0500

    fixed errs and find bar

--- server/BarBeerDrinker/__init__.py
+++ server/BarBeerDrinker/__init__.py
@@ -12,17 +12,17 @@ def get_bars():
 
 @app.route('/api/bar/<name>', methods=["GET"])
 def find_bar(name):
- try:
- if name is None:
- raise ValueError("Bar is not specified.")
- bar = database.find_bar(name)
- if bar is None:
- return make_response("No bar found with the given name.", 404)
- return jsonify(bar)
- except ValueError as e:
- return make_response(str(e), 400)
- except Exception as e:
- return make_response(str(e), 500)
+	try:
+		if name is None:
+			raise ValueError("Bar is not specified.")
+		bar = database.find_bar(name)
+		if bar is None:
+			 return make_response("No bar found with the given name.", 404)
+		return jsonify(bar)
+	except ValueError as e:
+		return make_response(str(e), 400)
+	except Exception as e:
+		return make_response(str(e), 500)
 
 @app.route('/api/beers_cheaper_than', methods=["POST"])
 def find_beers_cheaper_than():
